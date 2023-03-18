6573fc475eb98608a49641651af6a75a29f5bbf4
jasin1 <jasin.tairaidrissi@gmail.com>
2023-03-18 15:05:34 +0100

    Using attributes to get info selected bike

--- bike-rental.js
+++ bike-rental.js
@@ -1,5 +1,12 @@
 console.log("here we go!!");
 
+const selectedBike = {
+  type:"",
+  priceOneDay:"",
+  PriceExtra:"",
+  imgURL:""
+};
+
 const bikes = document.querySelectorAll(".main-bike-link");
 
 bikes.forEach((bike) => {
@@ -7,7 +14,15 @@ bikes.forEach((bike) => {
     event.preventDefault();
 
     const bikeType = bike.getAttribute("data-bike-type");
+    const bikePriceOneDay = bike.getAttribute("data-price-1-day");
+    const bikePriceExtra = bike.getAttribute("data-price-extra");
+    const bikeImgURL = bike.getAttribute("data-img-url");
 
-    console.log(`selected bike type: ${bikeType}`);
+    selectedBike.type = bikeType;
+    selectedBike.priceOneDay = bikePriceOneDay;
+    selectedBike.PriceExtra = bikePriceExtra;
+    selectedBike.imgURL = bikeImgURL;
+
+    console.log(selectedBike);
   });
 });
