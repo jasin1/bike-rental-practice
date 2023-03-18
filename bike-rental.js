8542494bcbc22d48a277e34a0591e4ae64689621
jasin1 <jasin.tairaidrissi@gmail.com>
2023-03-18 14:13:52 +0100

    capturing bikes

--- bike-rental.js
+++ bike-rental.js
@@ -1,1 +1,13 @@
 console.log("here we go!!");
+
+const bikes = document.querySelectorAll(".main-bike-link");
+
+bikes.forEach((bike) => {
+  bike.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    const bikeType = bike.getAttribute("data-bike-type");
+
+    console.log(`selected bike type: ${bikeType}`);
+  });
+});
