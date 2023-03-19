74980c291df710081d9e66dbb94640e97a3d850c
jasin1 <jasin.tairaidrissi@gmail.com>
2023-03-19 13:40:23 +0100

    adding steps logic

--- bike-rental.js
+++ bike-rental.js
@@ -1,5 +1,13 @@
 console.log("here we go!!");
 
+const form = {
+  currentStep:1,
+  steps: document.querySelectorAll('.tab'),
+  // nextButtons: document.querySelectorAll('[id^="next"]'),
+  prevButtons: document.querySelectorAll('.prev-button'),
+  submitButton: document.getElementById('submit')
+};
+
 const selectedBike = {
   type:"",
   priceOneDay:"",
@@ -23,6 +31,52 @@ bikes.forEach((bike) => {
     selectedBike.PriceExtra = bikePriceExtra;
     selectedBike.imgURL = bikeImgURL;
 
+    const currentStep = document.querySelector('.tab.active');
+    const nextStep = document.querySelector(`.tab[data-step="${form.currentStep + 1}"]`);
+
+    currentStep.classList.remove('active');
+    currentStep.classList.add('hidden');
+
+    nextStep.classList.remove('hidden');
+    nextStep.classList.add('active');
+
+    form.currentStep++;
+
     console.log(selectedBike);
   });
 });
+
+
+const nextButton = document.getElementById('next');
+
+nextButton.addEventListener('click', (event) =>{
+  event.preventDefault();
+
+  const currentStep = document.querySelector('.tab.active');
+  const nextStep = document.querySelector(`.tab[data-step="${form.currentStep + 1}"]`);
+
+  currentStep.classList.remove('active');
+  currentStep.classList.add('hidden');
+
+  nextStep.classList.remove('hidden');
+  nextStep.classList.add('active');
+
+  form.currentStep++;
+});
+
+form.prevButtons.forEach(button =>{
+  button.addEventListener('click', (event)=>{
+    event.preventDefault();
+
+    const currentStep = document.querySelector('.tab.active');
+    const nextStep = document.querySelector(`.tab[data-step="${form.currentStep - 1}"]`);
+
+    currentStep.classList.remove('active');
+    currentStep.classList.add('hidden');
+  
+    nextStep.classList.remove('hidden');
+    nextStep.classList.add('active');
+  
+    form.currentStep--;
+  })
+})
