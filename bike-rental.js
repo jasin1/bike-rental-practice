27e22701e6709932767b01e24c151a75934dc58e
jasin1 <jasin.tairaidrissi@gmail.com>
2023-04-05 08:39:04 +0200

    Commented Gsap

--- bike-rental.js
+++ bike-rental.js
@@ -126,8 +126,8 @@ bikes.forEach((bike) => {
       `.tab[data-step="${form.currentStep + 1}"]`,
     );
 
-    gsap.fromTo(currentStep,{opacity: 1, y:0},{opacity:0, y:50, duration:0.5});
-    gsap.fromTo(nextStep,{opacity: 0, y:50},{opacity:0, y:0, duration:0.5});
+   // gsap.fromTo(currentStep,{opacity: 1, y:0},{opacity:0, y:50, duration:0.5});
+    //gsap.fromTo(nextStep,{opacity: 0, y:50},{opacity:0, y:0, duration:0.5});
 
 
     currentStep.classList.remove("active");
