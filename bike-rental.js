e599ed7996d39ab19434f047f48eb9b3c82c647a
jasin1 <jasin.tairaidrissi@gmail.com>
2023-04-05 09:54:58 +0200

    removed GSAP

--- bike-rental.js
+++ bike-rental.js
@@ -126,12 +126,6 @@ bikes.forEach((bike) => {
       `.tab[data-step="${form.currentStep + 1}"]`,
     );
 
-    const tl = gsap.timeline();
-
-    tl.fromTo(currentStep, {opacity: 1, height: "auto"}, {opacity: 0, height: 0, duration: 0.3});
-
-
-    tl.add(()=>{
     currentStep.classList.remove("active");
     currentStep.classList.add("hidden");
 
@@ -139,10 +133,6 @@ bikes.forEach((bike) => {
     nextStep.classList.add("active");
 
     form.currentStep++;
-    });
-
-    tl.fromTo(nextStep, {opacity: 0, height: 0}, {opacity: 1, height: "auto", duration: 0.3});
-
   });
 });
 
@@ -156,13 +146,8 @@ form.prevButtons.forEach((button) => {
     const nextStep = document.querySelector(
       `.tab[data-step="${form.currentStep - 1}"]`,
     );
+    
 
-    const timeline = gsap.timeline();
-
-    timeline.fromTo(currentStep, { height: 'auto', opacity: 1 }, { height: 0, opacity: 0 });
-    timeline.fromTo(nextStep, { height: 0, opacity: 0 }, { height: 'auto', opacity: 1 });
-
-    timeline.add(() => {
     currentStep.classList.remove("active");
     currentStep.classList.add("hidden");
 
@@ -170,8 +155,6 @@ form.prevButtons.forEach((button) => {
     nextStep.classList.add("active");
 
     form.currentStep--;
-    });
-
   });
 });
 
