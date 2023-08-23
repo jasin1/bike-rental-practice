78ce52df43fb81c5c857a426ea3cc6a286d4e76d
jasin1 <jasin.tairaidrissi@gmail.com>
2023-08-23 16:04:32 +0200

    value

--- bike-rental.js
+++ bike-rental.js
@@ -71,7 +71,7 @@ const collectDays = document.getElementById("collect-days");
 const collectPrice = document.getElementById("collect-price");
 const collectItems = document.getElementById("collect-items");
 
-collectTime.innerText = "ready";
+collectTime.value = "change this!"
 
 //--------------- Bike selection ---------------------//
 
